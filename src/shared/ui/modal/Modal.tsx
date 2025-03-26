6a2ae50896f13040acd2ea5886dca0757e2832ac
Andrew <xpynkuy3@gmail.com>
2025-03-26 17:53:01 +0500

    add modal and fix styles

--- src/shared/ui/modal/Modal.tsx
+++ src/shared/ui/modal/Modal.tsx
@@ -1,5 +1,6 @@
-import { FC, ReactNode } from "react";
+import { FC, MouseEvent, ReactNode } from "react";
 import styles from "./Modal.module.scss";
+import { X } from "lucide-react";
 
 interface ModalProps {
   children: ReactNode;
@@ -8,10 +9,19 @@ interface ModalProps {
 }
 
 const Modal: FC<ModalProps> = ({ children, isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  const handleClickOverlay = (e: MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
     <div className={styles.modal}>
-      <div className={styles.overlay}>
-        <div className={styles.content}>{children}</div>
+      <div className={styles.overlay} onClick={handleClickOverlay}>
+        <div className={styles.content}>
+          <button className={styles.closeBtn} onClick={onClose}><X/></button>{children}</div>
       </div>
     </div>
   );
