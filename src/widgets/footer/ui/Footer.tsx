797586fbdc3fb7eaecddfe1e20f8ed1eecc96d89
Andrew <xpynkuy3@gmail.com>
2025-03-31 16:59:22 +0500

    fix footer comp

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,3 +1,24 @@
+import { Instagram, Twitter, Youtube } from "lucide-react";
+import styles from "./Footer.module.scss";
 export const Footer = () => {
-  return <div>FOOTER</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        <span className={styles.logo}>Kurosaw.com</span>
+        <span className={styles.link}>Tearms & Privacy</span>
+        <span className={styles.link}>Contacts</span>
+      </div>
+      <div className={styles.social}>
+        <span className={styles.icon}>
+          <Instagram />
+        </span>
+        <span className={styles.icon}>
+          <Twitter />
+        </span>
+        <span className={styles.icon}>
+          <Youtube />
+        </span>
+      </div>
+    </footer>
+  );
 };
