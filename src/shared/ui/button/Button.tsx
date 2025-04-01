c7ae5a34b3b6e7e657dc45fd446ef219044fc8bc
Andrew <xpynkuy3@gmail.com>
2025-04-01 11:22:49 +0500

    add mixins for animations

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -4,10 +4,11 @@ import styles from './Button.module.scss'
 
 interface ButtonProps {
   children: ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button: FC<ButtonProps> = ({ children }) => {
-  return <button className={styles.btn}>
+const Button: FC<ButtonProps> = ({ children, onClick }) => {
+  return <button className={styles.btn} onClick={onClick}>
     {children}
   </button>
 };
