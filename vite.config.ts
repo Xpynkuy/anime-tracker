9c5fad03b23cdd5b04a3c15818a9ee4c46ff5c10
Andrew <xpynkuy3@gmail.com>
2025-04-03 21:13:29 +0500

    fix json/config files

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Получаем __dirname "по-современному"
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -11,12 +11,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      'App': path.resolve(__dirname, 'src/App'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@widgets': path.resolve(__dirname, 'src/widgets'),
       '@shared': path.resolve(__dirname, 'src/shared'),
       '@features': path.resolve(__dirname, 'src/features'),
       '@entities': path.resolve(__dirname, 'src/entities'),
-      '@app': path.resolve(__dirname, 'src/app'),
     },
   },
 });
