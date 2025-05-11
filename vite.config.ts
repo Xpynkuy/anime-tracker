9768740a762f7476ad43ba4056e6de2770707190
Andrew <xpynkuy3@gmail.com>
2025-05-11 22:31:44 +0500

    add  rollup plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    visualizer({
+      open: true, // Автоматически откроет отчет в браузере
+      filename: 'bundle-stats.html', // Имя файла отчета
+      gzipSize: true, // Показывать размер после gzip
+      brotliSize: true,
+      template: 'treemap'
+    }),
+  ],
+  build: {
+    sourcemap: true
+  },
   resolve: {
     alias: {
       'App': path.resolve(__dirname, 'src/App'),
