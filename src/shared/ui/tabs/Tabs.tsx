502becc0853d3b0d5e6a9973b2a8f22a2641b860
Andrew <xpynkuy3@gmail.com>
2025-06-10 21:41:25 +0500

    create new shared comp (tabs)

---
+++ src/shared/ui/tabs/Tabs.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import styles from "./Tabs.module.scss";
+
+interface TabsItem {
+  id: string;
+  label: string;
+}
+
+interface TabsProps {
+  tabs: TabsItem[];
+  defaultActiveTab?: string;
+  children: (tab: string) => React.ReactNode;
+}
+
+const Tabs = ({ tabs, defaultActiveTab, children }: TabsProps) => {
+  const [activeTab, setActiveTab] = useState(defaultActiveTab || tabs[0].id);
+
+  return (
+    <div className={styles.tabsContainer}>
+      <div className={styles.tabsHeader}>
+        {tabs.map((tab) => (
+          <button
+            key={tab.id}
+            onClick={() => setActiveTab(tab.id)}
+            className={`${styles.tabButton} ${
+              activeTab === tab.id ? styles.activeTab : ""
+            }`}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className={styles.tabsContent}>{children(activeTab!)}</div>
+    </div>
+  );
+};
+
+export default Tabs;
